6e707504a3b3664756eb16c8a8b11329e89ef676
marmaladejam <166279529+marmaladejam@users.noreply.github.com>
2025-06-14 21:38:47 +1000

    fix home row mods

--- rules.mk
+++ rules.mk
@@ -2,12 +2,11 @@ CONVERT_TO = rp2040_ce
 
 LAYER_LOCK_ENABLE = yes
 EXTRAKEY_ENABLE = yes
-TAP_DANCE_ENABLE = yes
 CAPS_WORD_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes
+# KEY_OVERRIDE_ENABLE = yes
 
 MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+#RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+OLED_DRIVER     = ssd1306
 LTO_ENABLE      = yes
